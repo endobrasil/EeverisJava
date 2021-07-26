717b771ad3ab124ffb9523af66b4365387e8801f
AndreWendel <endobrasil@yahoo.com.br>
2021-07-26 18:03:54 -0300

    primeiro commit

---
+++ src/teste/teste.java
@@ -0,0 +1,28 @@
+package teste;
+
+import br.com.dio.model.Gato;
+
+public class teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Ola");
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		
+
+	}
+
+}
+
+
+class Livros{
+	private String nome;
+	private String npag;	
+	
+}
